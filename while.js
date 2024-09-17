75f70af7d952035b555a1d1d602c55473c550063
sleyaa <zarekparchmont14@gmail.com>
2024-09-17 17:10:12 -0600

    Add files via upload

---
+++ while.js
@@ -0,0 +1,7 @@
+//declarar variable 
+let num = 0;
+
+while (num <= 100){
+    num = parseInt(prompt("Ingrese un numero mayor que 100: "));
+}
+document.write("Numero ingresado mayor que 100: ", num);
